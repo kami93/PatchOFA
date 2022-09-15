b8d76f8c6b21719745b985539680266a4b6bd4e8
kami93 <qkrtjdgus93@gmail.com>
2022-09-15 22:02:00 +0900

    minor name fix

--- criterions/__init__.py
+++ criterions/__init__.py
@@ -3,7 +3,7 @@ from .label_smoothed_cross_entropy import AdjustLabelSmoothedCrossEntropyCriteri
 from .clip_scst_loss import ClipScstRewardCriterion
 from .label_smoothed_encouraging_loss import AdjustLabelSmoothedEncouragingLossCriterion
 
-from .label_smoothed_cross_entropy_for_maskedofa import AdjustLabelSmoothedCrossEntropyCriterionMaskedOFA
+from .label_smoothed_cross_entropy_for_masked_ofa import AdjustLabelSmoothedCrossEntropyCriterionMaskedOFA
 
 # from .patch_ofa_custom_criterion_v1 import CustomCriterionV1
 # from .patch_ofa_custom_criterion_v2 import CustomCriterionV2
